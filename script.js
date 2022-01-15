6bcba05876a325122e9a1569c75fd6a986a9734e
rinuya <rinuya@users.noreply.github.com>
2022-01-15 16:43:13 +0100

    managed to get it to regular draw, will implement mousebutton1 down to draw aswell, a clearing button that removes the class when pressed, and a color change of 10% instead of direct.

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const main = document.querySelector(".main");
 
-const row = document.createElement("div");
-main.appendChild(row);
-const square = document.createElement("div");
-row.appendChild(square);
+// const row = document.createElement("div");
+// main.appendChild(row);
+// const square = document.createElement("div");
+// row.appendChild(square);
+
+// row.setAttribute("class", "row");
+// square.setAttribute("class", "square");
+
+const i = 100;
+
+function generate (squareRowsAndColumns) {
+    for (let y=0; y<=squareRowsAndColumns; y++) {
+        const row = document.createElement("div");
+        main.appendChild(row);
+        row.setAttribute("id", "row");
+
+        for (let x=0; x<=squareRowsAndColumns; x++) {
+            const square = document.createElement("div");
+            row.appendChild(square);
+            square.setAttribute("id", "square");
+            square.addEventListener('mouseout', () => {
+                square.setAttribute('class', "black")
+                // alert(square.class);
+            });
+        }
+    }
+}
+generate(i);
+
+// const element = document.getElementsByClassName("square");
+// console.log(element);
+// element.onhover = changeColor(element);
+function changeColor(self) {
+// var black = '#000000'
+    self.style.backgroundColor = "black";
+};
+
+//edit padding depending on website lenght: website lenght *0,8/ squares
+
