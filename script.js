47e3bcaff7d1779d2e210f0e6ddc9b028d3d2b68
rinuya <rinuya@users.noreply.github.com>
2022-01-16 11:47:19 +0100

    Finished, changing into RGB and changing Opacity is unnecessary, and somewhat easy

--- script.js
+++ script.js
@@ -1,58 +1,45 @@
 const main = document.querySelector(".main");
 const btn = document.querySelector("#clear");
-// const row = document.createElement("div");
-// main.appendChild(row);
-// const square = document.createElement("div");
-// row.appendChild(square);
 
-// row.setAttribute("class", "row");
-// square.setAttribute("class", "square");
-
-
-//   IISSSSUUEEESS HEEERREEE
 btn.onclick = () => {
-    let remove = document.querySelectorAll(".black")
-    if (remove>0){
-    remove.classList.remove("black");
-    console.log(remove)
+    let rowRemove = document.querySelectorAll(".row");
+    //for each row removes one child from main (so all the rows)
+    rowRemove.forEach(child=>{main.removeChild(child)});
     let size = prompt("How big do you want your drawing board to be? Max size is 100!");
     if (size<=100 && size>0){
         generate(size);
     }
     else{
-        alert("Error, please try again with a full Number");
+        alert("Error, please try again with a full number");
     }
-}
 }
 
 const i = 32;
 
 function generate (squareRowsAndColumns) {
-    for (let y=0; y<=squareRowsAndColumns; y++) {
+    for (let y=0; y<squareRowsAndColumns; y++) {
         const row = document.createElement("div");
         main.appendChild(row);
-        row.setAttribute("id", "row");
+        row.setAttribute("class", "row");
 
-        for (let x=0; x<=squareRowsAndColumns; x++) {
+        for (let x=0; x<squareRowsAndColumns; x++) {
             const square = document.createElement("div");
             row.appendChild(square);
-            square.setAttribute("id", "square");
+            square.setAttribute("class", "square");
+           
             square.addEventListener('mouseout', () => {
-                square.setAttribute('class', "black")
-                // alert(square.class);
+                square.setAttribute('id', "black")
             });
-        }
+        }    
     }
+    const rowsHelperFunction = document.getElementsByClassName("row");
+        const amount = 1/(rowsHelperFunction.length) * 940;
+
+    const squareChange = document.getElementsByClassName("square");
+    Array.from(rowsHelperFunction).forEach(el => 
+        el.style.height = amount + "px");
+    Array.from(squareChange).forEach(el =>
+        el.style.width = amount + "px");
+    
 }
 generate(i);
-
-// const element = document.getElementsByClassName("square");
-// console.log(element);
-// element.onhover = changeColor(element);
-function changeColor(self) {
-// var black = '#000000'
-    self.style.backgroundColor = "black";
-};
-
-//edit padding depending on website lenght: website lenght *0,8/ squares
-
